c472fbd3180843cf1d982a12817969caaa0c09d9
Luthfi <luthfiapriyanto@gmail.com>
2019-10-28 00:29:03 +0700

    Updates

---
+++ precache-manifest.9b0695acf0bc84a66978b99200b2e7cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc397d7f6b3bebc95477",
+    "url": "/kakado/static/css/main.1bfdcf86.chunk.css"
+  },
+  {
+    "revision": "bc397d7f6b3bebc95477",
+    "url": "/kakado/static/js/main.bc397d7f.chunk.js"
+  },
+  {
+    "revision": "7bc0968a838e134d62d9",
+    "url": "/kakado/static/js/1.7bc0968a.chunk.js"
+  },
+  {
+    "revision": "ea29b22395cb05546355",
+    "url": "/kakado/static/js/runtime~main.ea29b223.js"
+  },
+  {
+    "revision": "7f5de808c174be4c40627ead4d591d49",
+    "url": "/kakado/static/media/brand_logo.7f5de808.png"
+  },
+  {
+    "revision": "35b73e2137f97ea4b566b8a3c2d1d7ef",
+    "url": "/kakado/static/media/landing_header.35b73e21.jpg"
+  },
+  {
+    "revision": "1caf0984ef7368ba889cb6c69d92e064",
+    "url": "/kakado/static/media/recipient_gift.1caf0984.jpg"
+  },
+  {
+    "revision": "dc0b49dd7da0a75f0fb65260abfc6d50",
+    "url": "/kakado/index.html"
+  }
+];
